5c260c929eaace6d08484658de068423e5fb78eb
JiaqiJia <jiajiaqisb@126.com>
2019-11-13 19:03:37 +0800

    增加了axios的内容

--- script/nd/test.js
+++ script/nd/test.js
@@ -1,1 +1,16 @@
-console.log("Nodejs test");
+var http = require('http');
+var fs = require('fs');
+
+var myServer = http.createServer(function (req, res) {
+    var html = fs.readFileSync('app/axioslearn.html');
+    res.write(html);
+    res.end();
+})
+
+myServer.listen('8988', function (err) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+    console.log("The service is open and running.");
+})
